daec0a46780cae17c0c4d73e5a109d8065e35662
Joanna <jm.ziemska@gmail.com>
2025-10-22 13:55:24 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-python3 ./count_genes.py --in_folder /data02/maria/kwasy/shotgun_BSH/ --out_file ./result.csv --preffix_files mgshot_
-python3 ./merge_by_cd_hit_res.py --out_file ./result_merged.csv --count_genes_file ./result.csv --cd_hit_res /data02/maria/kwasy/shotgun_BSH/clusters.clstr
